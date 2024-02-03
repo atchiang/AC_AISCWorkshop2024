d87365200826f5df176d36989c05d23af7217183
atchiang <156946894+atchiang@users.noreply.github.com>
2024-02-03 15:48:32 -0800

    wkshp COMMIT

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import IndexMSG from "./_components/indexMSG"
+
+export default function Home(){
+    return (
+        <div>
+            <h1>HALLO Peeps</h1>
+            <IndexMSG></IndexMSG>
+        </div>
+    )
+}
